dbd2f21404fe4fcc2facee0d7c667c47a34ce04c
dpadilla90 <d_padilla.y@hotmail.com>
2023-11-08 19:48:58 -0500

    Implement NSCache extension for URL-based CacheEntry retrieval

---
+++ ForecastFavorApp_iOS/ForecastFavorApp_iOS/NSCache+Subscript.swift
@@ -0,0 +1,27 @@
+//
+//  NSCache+Subscript.swift
+//  ForecastFavorApp_iOS
+//
+//  Created by Daphne Rivera on 2023-11-08.
+//
+
+import Foundation
+
+extension NSCache where KeyType == NSString, ObjectType == CacheEntryObject {
+    subscript(_ url: URL) -> CacheEntry? {
+        get {
+            let key = url.absoluteString as NSString
+            let value = object(forKey: key)
+            return value?.entry
+        }
+        set{
+            let key = url.absoluteString as NSString
+            if let entry = newValue {
+                let value = CacheEntryObject(entry: entry)
+                setObject(value, forKey: key)
+            } else {
+                removeObject(forKey: key)
+            }
+        }
+    }
+}
