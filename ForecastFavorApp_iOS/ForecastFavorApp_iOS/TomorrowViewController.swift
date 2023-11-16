ba7bd3ec0ffdaef869afddbc6e1bdff3454ed3de
dpadilla90 <d_padilla.y@hotmail.com>
2023-11-15 22:39:13 -0500

    Refactor TomorrowViewController to use UISearchBar for fetching forecast

--- ForecastFavorApp_iOS/ForecastFavorApp_iOS/TomorrowViewController.swift
+++ ForecastFavorApp_iOS/ForecastFavorApp_iOS/TomorrowViewController.swift
@@ -7,10 +7,10 @@
 
 import UIKit
 
-class TomorrowViewController: UIViewController {
+class TomorrowViewController: UIViewController,  UISearchBarDelegate  {
 
     // Outlets for your UI components
-    @IBOutlet weak var locationTextField: UITextField!
+
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var conditionLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
@@ -20,10 +20,12 @@ class TomorrowViewController: UIViewController {
     @IBOutlet weak var chanceOfRainLabel: UILabel!
     @IBOutlet weak var chanceOfSnowLabel: UILabel!
     @IBOutlet weak var uvLabel: UILabel!
+    @IBOutlet weak var searchBar: UISearchBar!
     // ... other labels for additional forecast details
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.delegate = self // Set the search bar delegate
         conditionLabel.text = ""
         temperatureLabel.text = "Current temp: "
         chanceOfRainLabel.text = "Chance of rain: "
@@ -34,27 +36,32 @@ class TomorrowViewController: UIViewController {
         uvLabel.text = "UV Index: "
         // Set initial states for your labels, image views, etc.
     }
+    // This method is called when the user taps the search button on the keyboard
+       func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+           searchBar.resignFirstResponder() // Hide the keyboard
 
-    @IBAction func fetchTomorrowForecastTapped(_ sender: Any) {
-        guard let location = locationTextField.text, !location.isEmpty else {
-            // Handle empty text field case
-            return
-        }
-        
-        Task {
-            do {
-                // Asynchronously fetch the forecast data for the next day
-                let forecastData = try await WeatherAPI_Helper.fetchForecastData(cityName: location, days: 1)
-                // Make sure to safely unwrap the array element to avoid potential index out of range error
-                if let tomorrowForecast = forecastData.forecast?.forecastday[1] {
-                    await updateUI(with: tomorrowForecast)
-                }
-            } catch {
-                // Handle errors, perhaps show an alert to the user
-            }
-        }
-    }
+           guard let cityName = searchBar.text, !cityName.isEmpty else {
+               // Optionally, inform the user that the search bar is empty
+               return
+           }
 
+           fetchWeatherForCity(cityName)
+       }
+
+       private func fetchWeatherForCity(_ cityName: String) {
+           Task {
+               do {
+                   let forecastData = try await WeatherAPI_Helper.fetchForecastData(cityName: cityName, days: 1)
+                   // Index should be 0 for the first element which is tomorrow's forecast
+                   if let tomorrowForecast = forecastData.forecast?.forecastday.first {
+                       await updateUI(with: tomorrowForecast)
+                   }
+               } catch {
+                   // Handle errors, perhaps by showing an alert with the error description
+                   print("Error fetching forecast: \(error.localizedDescription)")
+               }
+           }
+       }
     // Ensure updateUI is marked as async to perform async operations
     @MainActor
     private func updateUI(with forecastDay: ForecastdayContainer) async {
