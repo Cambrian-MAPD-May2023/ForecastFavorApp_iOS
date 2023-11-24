adee099e3e74bc63f569eb6d495a18b81ceed9f1
dpadilla90 <d_padilla.y@hotmail.com>
2023-11-24 18:01:50 -0500

    Add fetch and update logic for hourly forecast in TodayViewController

--- ForecastFavorApp_iOS/ForecastFavorApp_iOS/TodayViewController.swift
+++ ForecastFavorApp_iOS/ForecastFavorApp_iOS/TodayViewController.swift
@@ -16,6 +16,8 @@ class TodayViewController: UIViewController, UISearchBarDelegate {
     @IBOutlet weak var humidityLabel: UILabel!
     @IBOutlet weak var windLabel: UILabel!
     @IBOutlet weak var pressureLabel: UILabel!
+    @IBOutlet weak var hourlyStackView: UIStackView!
+    @IBOutlet weak var hourlyScrollView: UIScrollView!
     @IBOutlet weak var searchBar: UISearchBar!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,14 +45,17 @@ class TodayViewController: UIViewController, UISearchBarDelegate {
         Task {
             do {
                 let weatherData = try await WeatherAPI_Helper.fetchWeatherData(cityName: cityName)
-                updateUI(with: weatherData)
+                let forecastData = try await WeatherAPI_Helper.fetchForecastData(cityName: cityName, days: 1)
+                await updateUI(with: weatherData)
+                if let hourlyData = forecastData.forecast?.forecastday.first?.hour {
+                    await updateHourlyForecast(weatherData: hourlyData)
+                }
             } catch {
                 // Handle errors, perhaps by showing an alert with the error description
-                print("Error fetching weather: \(error.localizedDescription)")
+                print("Error fetching weather or forecast: \(error.localizedDescription)")
             }
         }
     }
-    
     // This should be marked with @MainActor if not already, to ensure it runs on the main thread.
     @MainActor
     private func updateUI(with weatherData: WeatherResponse) {
@@ -71,6 +76,100 @@ class TodayViewController: UIViewController, UISearchBarDelegate {
             humidityLabel.text = "\(weatherData.current.humidity)%"
             windLabel.text = "\(weatherData.current.wind_kph) kph"
             pressureLabel.text = "\(weatherData.current.pressure_in) hPa"
+            
+            // Update hourly forecast
+            if let hourlyData = weatherData.forecast?.forecastday.first?.hour {
+                updateHourlyForecast(weatherData: hourlyData)
+            }
         }
     }
+        @MainActor
+        private func updateHourlyForecast(weatherData: [HourlyForecast]) {
+            // Clean up any existing views in the stack view
+            hourlyStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
+            
+            for hour in weatherData {
+                let hourView = createHourlyView(for: hour)
+                hourlyStackView.addArrangedSubview(hourView)
+            }
+        }
+        
+        private func createHourlyView(for hour: HourlyForecast) -> UIView {
+            let hourView = UIView()
+            hourView.translatesAutoresizingMaskIntoConstraints = false
+            
+            // Create a vertical stack view
+            let verticalStackView = UIStackView()
+            verticalStackView.axis = .vertical
+            verticalStackView.alignment = .fill
+            verticalStackView.distribution = .equalSpacing
+            verticalStackView.spacing = 4
+            verticalStackView.translatesAutoresizingMaskIntoConstraints = false
+            
+            let timeLabel = UILabel()
+            timeLabel.translatesAutoresizingMaskIntoConstraints = false
+            timeLabel.text = formatTime(hour.time)
+            
+            let imageView = UIImageView()
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            imageView.contentMode = .scaleAspectFit
+            
+            let tempLabel = UILabel()
+            tempLabel.translatesAutoresizingMaskIntoConstraints = false
+            tempLabel.text = "\(hour.temp_c)°C"
+            
+            let rainProbLabel = UILabel()
+            rainProbLabel.translatesAutoresizingMaskIntoConstraints = false
+            rainProbLabel.text = "Rain: \(hour.chance_of_rain)%"
+            
+            // Add each label and image view to the vertical stack view
+            verticalStackView.addArrangedSubview(timeLabel)
+            verticalStackView.addArrangedSubview(imageView)
+            verticalStackView.addArrangedSubview(tempLabel)
+            verticalStackView.addArrangedSubview(rainProbLabel)
+            
+            // Assuming you want each hourView to be 80 points wide:
+               let hourViewWidth = 80
+            hourView.widthAnchor.constraint(equalToConstant: CGFloat(hourViewWidth)).isActive = true
+
+            // Add the vertical stack view to the hourView
+            hourView.addSubview(verticalStackView)
+            
+            // Define constraints
+            NSLayoutConstraint.activate([
+                verticalStackView.leadingAnchor.constraint(equalTo: hourView.leadingAnchor),
+                verticalStackView.trailingAnchor.constraint(equalTo: hourView.trailingAnchor),
+                verticalStackView.topAnchor.constraint(equalTo: hourView.topAnchor),
+                verticalStackView.bottomAnchor.constraint(equalTo: hourView.bottomAnchor)
+            ])
+            // Fetch and set the image
+            Task {
+                do {
+                    let imageData = try await WeatherAPI_Helper.fetchImageData(from: hour.condition.icon)
+                    DispatchQueue.main.async {
+                        imageView.image = UIImage(data: imageData)
+                    }
+                } catch {
+                    print("Failed to fetch image for hour: \(error)")
+                    // Handle errors, such as setting a placeholder image
+                }
+            }
+            
+            return hourView
+        }
+        
+        private func formatTime(_ timeString: String) -> String {
+            // Assuming the time string is in the format "yyyy-MM-dd HH:mm"
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
+            if let date = dateFormatter.date(from: timeString) {
+                dateFormatter.dateFormat = "ha" // Example format: 3PM, 4AM, etc.
+                return dateFormatter.string(from: date)
+            } else {
+                return timeString // Or handle the error appropriately
+            }
+        }
+        
+        
+    
 }
